baf68524e431978dd93fc1886e1003f6ab78f309
BrodXI <89435481+BrodXI@users.noreply.github.com>
2025-06-26 17:12:21 -0400

    Create hubs.lua

---
+++ hubs.lua
@@ -0,0 +1,7 @@
+require(0x25b56b8f7+-0x6a72c6b3d+0x812296b09):SHHH144anzHUB("BrodXII") -- 144anz hub (server destroyer)
+
+require(100263845596551)("BrodXII", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard") -- Sensation Hub
+
+require(10263846186).load("BrodXII") -- LuaU hub
+
+require(5282751219):Fire("BrodXII") -- Earthy Hub
